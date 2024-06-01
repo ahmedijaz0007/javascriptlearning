3783e6dfbaf2bcfd96c2b9d3f0b3073152214510
Ahmed Ijaz <ahmedijaz0007@gmail.com>
2024-06-01 11:28:35 -0700

    find occurances completed

--- findOccurances.js
+++ findOccurances.js
@@ -3,11 +3,11 @@ let arr = [2,2,3,3,4,4,4]
 let occurrences = {}
 
 for (let i =0; i<arr.length;i++){
-    if (`${arr[i]}` in occurrences){
-    occurrences[`${arr[i]}`] += 1; 
+    if (arr[i] in occurrences){
+    occurrences[arr[i]] += 1; 
     }
     else {
-        occurrences[`${arr[i]}`] = 1;
+        occurrences[arr[i]] = 1;
     }
 }
 
